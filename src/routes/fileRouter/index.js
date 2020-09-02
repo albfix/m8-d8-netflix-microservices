0dcd0754e3c68ce4d51bb68aa90f7a865136d533
Alberto Gaias <63840357+albfix@users.noreply.github.com>
2020-09-02 19:00:20 +0200

    blob + credentials, router, env + key

--- src/routes/fileRouter/index.js
+++ src/routes/fileRouter/index.js
@@ -1,5 +1,18 @@
 const express = require("express")
+const { BlobServiceClient, StorageSharedKeyCredential } = require("@azure/storage-blob")
+require("dotenv").config()
+
+const credentials = new StorageSharedKeyCredential("netflixmicroservices", process.env.STORAGE_KEY)
+const blobClient = new BlobServiceClient("http://netflixmicroservices.blob.core.windows.net/", credentials)
 
 const router = express.Router()
+
+router.get("/", async (req, res) => {
+    const containers = await blobClient.listContainers()
+    const toReturn = []
+    for await (const container of containers)
+        toReturn.push(container.name)
+    res.send(toReturn)
+})
 
 module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require("express")
+const fileRouter = require("./routes/fileRouter")
+require("dotenv").config()
 
 const app = express()
+
+app.use("/files", fileRouter)
 
 app.listen(process.env.PORT || 4301, () => {
     console.log(`Attivo nella porta ${process.env.PORT || 4301}`)
